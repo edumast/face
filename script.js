cd9ddfaa8e4112c8f01b0e523a6f18addadb2d9b
Edu_Kenji <86361043+edumast@users.noreply.github.com>
2023-09-02 19:35:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
 function finder_kind(rad_tp){
-  let radios=document.getElementById("radios" );
-  if (radios.checked){
+  let radios=document.getElementById("radios");
+  if ( rad_tp != "" && rad_tp != " " ){
+  //if (radios.checked){
     find_all(rad_tp)
-}else{
-    find(rad_tp)
-}
-}
+//}else{
+    //find(rad_tp)
+//}
+}} 
 
-        function find(data){
+        /*function find(data){
           let divs = document.getElementsByClassName("tempo");
             for(let i=0;i<divs.length;i++){
                 let n=divs[i].innerText;
@@ -31,6 +32,7 @@ function finder_kind(rad_tp){
                 }
             }
         }
+        */
        
        function find_all(data){
               let divs = document.getElementsByClassName("rd2");
@@ -55,15 +57,14 @@ function finder_kind(rad_tp){
   `
   		<div id="caixa"> 
 	<h1 style="text-align: center;">Pesquisa</h1>
+
 	  <input type="text" id="inp">
+
 	  <button class="button-1" onclick="finder_kind(document.getElementById('inp').value)">
 	    Buscar
 	    </button>
-	    <div class="type_center">
-	    	    <br>Busca por texto:<input type="radio" id="radios" name="radios"><br>
-	    	    *O padrão é data 
-	    	    </div>
-	    <h4 style="text-align:center;">DE 13/06/2023 A 23/07/2022</h4>
+	 		  <div class='type_center'>palavras e datas</div>
+	    <h4 style="text-align:center;">Parte 2: DE 14/06/2023 A indefinido<br><br>Parte 1: DE 13/06/2023 A 23/07/2022</h4>
 	  </div>
  
   `;
@@ -77,3 +78,6 @@ finder_activated=false;
          } 
         
         }
+        
+        /*<div class="type_center"><br>Busca por texto:<input type="radio" id="radios" name="radios"><br>*O padrão é data</div>*/
+	    	    
